7d464bbc8d705c58c19fd366811b4a06fabc7d08
viviana rishe <vivianacuri@gmail.com>
2017-03-13 14:43:26 -0700

    fixes references for users and groups

--- models/Occasion.js
+++ models/Occasion.js
@@ -5,8 +5,8 @@ var occasionSchema = new mongoose.Schema({
   title: {type: String, required: true},
   description: String,
   eventDate: Date,
-  users: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  groups: {type: mongoose.Schema.Types.ObjectId, ref: 'Group'}
+  users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  groups: [{type: mongoose.Schema.Types.ObjectId, ref: 'Group'}]
 });
 
 var Occasion = mongoose.model('Occasion', occasionSchema);
