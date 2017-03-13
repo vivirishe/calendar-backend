cb849f39db5c223823f61d8ddbbb0c9bf8ba68e6
viviana rishe <vivianacuri@gmail.com>
2017-03-13 11:22:06 -0700

    added mongoose, dotenv and config folder

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
+// Load env variables from .env file
+// require('dotenv').config();
+
+//variables for routes file
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+// var routes = require('.config/routes');
+
+//requires mongoose
+var mongoose = require('./config/database');
 
 var app = express();
 
@@ -16,6 +26,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
